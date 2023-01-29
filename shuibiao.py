d44487008796c48c54a2162e98551f1350b81c09
cubercsl <2014cais01@gmail.com>
2023-01-29 21:10:54 +0800

    chore: bump to ptb v20

--- shuibiao.py
+++ shuibiao.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import asyncio
 import base64
 import binascii
 import hashlib
 import logging
 import os
-import subprocess
 import random
 
-from telegram import Bot, Update, InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext import Updater, CommandHandler, \
-    InlineQueryHandler, ChosenInlineResultHandler, \
-    PicklePersistence
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
+from telegram.ext import (Application, CallbackContext,
+                          ChosenInlineResultHandler, CommandHandler,
+                          InlineQueryHandler, PicklePersistence)
 
-token = os.getenv('TELEGRAM_APITOKEN')
 asset_url = os.getenv('ASSET_URL', 'https://raw.githubusercontent.com/rocats/bot-collection-py/master/asset/shuibiao')
-data_path = os.getenv('DATA_PATH', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'shuibiao'))
 questions_file = os.getenv('QUESTIONS_PATH',
                            os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                         'asset', 'shuibiao', 'shuibiao.txt'))
-bot = Bot(token=token)
 
 questions_list = []
 questions_dict = {}
@@ -32,7 +28,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def init():
+def init() -> None:
     logger.info(f'Get question list from {questions_file}')
     with open(questions_file) as f:
         for line in f:
@@ -45,26 +41,26 @@ def init():
                 logger.warning('Load question error from \'%s\'', line)
 
 
-def error(update: Update, context: CallbackContext):
+async def error(update: Update, context: CallbackContext) -> None:
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
     print(context.error)
 
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text('跟我们走一趟')
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text('跟我们走一趟')
 
 
-def question(update: Update, context: CallbackContext):
+async def question(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
     reply_to_message_id = update.message.reply_to_message.message_id \
         if update.message.reply_to_message else None
     text = random.choice(questions_list).strip()
     context.bot_data[text] = context.bot_data.get(text, 0) + 1
-    context.bot.send_message(chat_id, text=text, reply_to_message_id=reply_to_message_id)
+    await context.bot.send_message(chat_id, text=text, reply_to_message_id=reply_to_message_id)
 
 
-def question_inline(update: Update, context: CallbackContext):
+async def question_inline(update: Update, context: CallbackContext) -> None:
     def get_stats():
         total = sum(context.bot_data.values())
         result = f'据不完全统计，目前全网累计查水表次数: {total} 次。其中排名前 3 的语句为：\n'
@@ -96,10 +92,10 @@ def question_inline(update: Update, context: CallbackContext):
             thumb_url=f'{asset_url}/shuibiao.jpg'
         )
     )
-    update.inline_query.answer(query_result, cache_time=0)
+    await update.inline_query.answer(query_result, cache_time=0)
 
 
-def chosen_result(update: Update, context: CallbackContext):
+async def chosen_result(update: Update, context: CallbackContext) -> None:
     result_id = update.chosen_inline_result.result_id
     if result_id == '00000000000000000000000000000000':
         return
@@ -107,40 +103,43 @@ def chosen_result(update: Update, context: CallbackContext):
     context.bot_data[text] = context.bot_data.get(text, 0) + 1
 
 
-def stats(update: Update, context: CallbackContext):
+async def stats(update: Update, context: CallbackContext) -> None:
     total = sum(context.bot_data.values())
     result = ""
     for count, text in sorted(list((-v, k) for k, v in context.bot_data.items())):
         result += f"{text}: {-count} 次\n"
     result += f"共查水表 {total} 次。"
-    update.message.reply_text(result)
+    await update.message.reply_text(result)
 
 
 def main():
-    bot.set_my_commands([
-        ('question', '我们建议你配合调查'),
-        ('stats', '查水表统计')
-    ])
-    persistence = PicklePersistence(filename=data_path)
-    updater = Updater(token, use_context=True, persistence=persistence)
-    dp = updater.dispatcher
+    token = os.getenv('TELEGRAM_APITOKEN')
+    data_path = os.getenv('DATA_PATH', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'shuibiao'))
+    persistence = PicklePersistence(filepath=data_path)
+
+    async def set_commands(app: Application):
+        await app.bot.set_my_commands([
+            ('question', '我们建议你配合调查'),
+            ('stats', '查水表统计')
+        ])
+
+    app = Application.builder() \
+        .persistence(persistence) \
+        .token(token) \
+        .post_init(set_commands) \
+        .build()
 
     # log all errors
-    dp.add_error_handler(error)
+    app.add_error_handler(error)
 
     # add handler
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("question", question))
-    dp.add_handler(CommandHandler("stats", stats))
-    dp.add_handler(InlineQueryHandler(question_inline))
-    dp.add_handler(ChosenInlineResultHandler(chosen_result))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("question", question))
+    app.add_handler(CommandHandler("stats", stats))
+    app.add_handler(InlineQueryHandler(question_inline))
+    app.add_handler(ChosenInlineResultHandler(chosen_result))
     # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    app.run_polling()
 
 
 if __name__ == '__main__':
--- tasks.py
+++ tasks.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import asyncio
 import logging
 import os
 import random
+from typing import Optional
 
-from telegram import Bot, Update, ParseMode
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext import (Updater, CommandHandler)
-
-token = os.getenv('TELEGRAM_APITOKEN')
-bot = Bot(token=token)
+from telegram import Bot, Update
+from telegram.constants import ParseMode
+from telegram.ext import Application, CallbackContext, CommandHandler
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -19,74 +18,83 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def error(update: Update, context: CallbackContext):
+async def error(update: Update, context: CallbackContext) -> None:
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
     print(context.error)
 
 
-def start(update: Update, context: CallbackContext):
+async def start(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
-    context.bot.send_message(chat_id, '`好`', parse_mode=ParseMode.MARKDOWN_V2)
+    await context.bot.send_message(chat_id, '`好`', parse_mode=ParseMode.MARKDOWN_V2)
 
 
-def ok(update: Update, context: CallbackContext, name: str = None):
+async def ok(update: Update, context: CallbackContext, name: Optional[str] = None) -> None:
     text = random.choice(("好的", "行", "可以", "完全同意", "OK", "我觉得好", "好吧", "嗯！"))
     if name is not None:
-        update.message.reply_text(f'{name}: {text}')
+        await update.message.reply_text(f'{name}: {text}')
     else:
         chat_id = update.message.chat_id
         reply_to_message_id = update.message.reply_to_message.message_id \
             if update.message.reply_to_message else None
-        context.bot.send_message(chat_id, text=text, reply_to_message_id=reply_to_message_id)
+        await context.bot.send_message(chat_id, text=text, reply_to_message_id=reply_to_message_id)
 
 
-def study(update: Update, context: CallbackContext):
+async def study(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
     reply_to_message_id = update.message.reply_to_message.message_id \
         if update.message.reply_to_message else None
     text = random.choice(("快去学习", "今天学了吗?", "今天学习了吗?", "啥时候学习?", "忘记学习了吗?", "快去学习啊"))
-    context.bot.send_message(chat_id, text=text, reply_to_message_id=reply_to_message_id)
+    await context.bot.send_message(chat_id, text=text, reply_to_message_id=reply_to_message_id)
 
 
-def assign(update: Update, context: CallbackContext):
+async def assign(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
     if update.message.reply_to_message is not None:
         reply_to_message_id = update.message.reply_to_message.message_id
-        context.bot.send_message(chat_id, '交 给 你 了', reply_to_message_id=reply_to_message_id)
-        ok(update, context, update.message.reply_to_message.from_user.full_name)
+        await context.bot.send_message(chat_id, '交 给 你 了', reply_to_message_id=reply_to_message_id)
+        await ok(update, context, update.message.reply_to_message.from_user.full_name)
     else:
-        context.bot.send_message(chat_id, f'{update.message.from_user.full_name}: 交 给 我 了')
+        await context.bot.send_message(chat_id, f'{update.message.from_user.full_name}: 交 给 我 了',
+                                       message_thread_id=update.message.message_thread_id)
 
 
-def unassign(update: Update, context: CallbackContext):
+async def unassign(update: Update, context: CallbackContext) -> None:
     name = update.message.reply_to_message.from_user.full_name \
         if update.message.reply_to_message is not None \
         else update.message.from_user.full_name
-    update.message.reply_text(f'{name}: 不 干 了')
+    await update.message.reply_text(f'{name}: 不 干 了')
 
 
 def main():
-    updater = Updater(token, use_context=True)
-    dp = updater.dispatcher
+
+    async def set_commands(app: Application):
+        await app.bot.set_my_commands([
+            ('assign', '交给你了'),
+            ('unassign', '不干了'),
+            ('ok', '好的，没问题'),
+            ('study', '快去学习')
+        ])
+
+    token = os.getenv('TELEGRAM_APITOKEN')
+    app = Application.builder().token(token) \
+        .post_init(set_commands) \
+        .build()
+
+
 
     # log all errors
-    dp.add_error_handler(error)
+    app.add_error_handler(error)
 
     # add handler
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("ok", ok))
-    dp.add_handler(CommandHandler("study", study))
-    dp.add_handler(CommandHandler("assign", assign))
-    dp.add_handler(CommandHandler("unassign", unassign))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("ok", ok))
+    app.add_handler(CommandHandler("study", study))
+    app.add_handler(CommandHandler("assign", assign))
+    app.add_handler(CommandHandler("unassign", unassign))
 
     # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    app.run_polling()
 
 
 if __name__ == '__main__':
